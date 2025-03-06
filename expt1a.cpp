e06c82982cbc86799658c21d33ab0353b177a2f6
Zainab Wani <146872966+Zainab-Wani@users.noreply.github.com>
2025-03-06 12:05:25 +0530

    Add files via upload

---
+++ expt1a.cpp
@@ -0,0 +1,16 @@
+/*Analyze and design classes for a student management system.
+Classes for student management system:-
+1.Student class
+To store and display student information.
+2.course class
+To store and display course details.
+3.Admin Class
+To change /Add/Delete/Update,information of student or course.
+4.Examination
+TO store and display information about te ongoing or upcoming exams.
+5.Result Class
+To store and result.
+6.Department Class
+To store and display information of all schools and departments of college.
+7.Notification Class
+To display all the latest information and updates.*/
